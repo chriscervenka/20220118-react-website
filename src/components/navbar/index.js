eae9ad8812afb13ae38b316e12f12221c99e4101
Christian Cervenka <christian.cervenka@oevp.at>
2022-01-19 10:49:21 +0100

    20220119-Commitment-1

---
+++ src/components/navbar/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { FaBars } from 'react-icons/fa'
+import {
+    Nav, 
+    NavbarContainer, 
+    NavLogo, MobileIcon, 
+    NavMenu, 
+    NavItem, 
+    NavLinks,
+    NavBtn,
+    NavBtnLink
+} from './navbarElements'
+
+const Navbar = () => {
+    return (
+        <>
+            <Nav>
+                <NavbarContainer>
+                    <NavLogo to='/' title='Logo | Dolla'>dolla</NavLogo>
+                    <MobileIcon>
+                        <FaBars />
+                    </MobileIcon>
+                    <NavMenu>
+                        <NavItem>
+                            <NavLinks to="about" title='About'>About</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="discover" title='Discover'>Discover</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="services" title='Services'>Services</NavLinks>
+                        </NavItem>
+                        <NavItem>
+                            <NavLinks to="signup" title='Sign Up'>Sign Up</NavLinks>
+                        </NavItem>
+                    </NavMenu>
+                    <NavBtn>
+                        <NavBtnLink to='/signin' title='Sign In'>Sign In</NavBtnLink>
+                    </NavBtn>
+                </NavbarContainer>
+            </Nav>
+        </>
+    );
+};
+
+export default Navbar;
